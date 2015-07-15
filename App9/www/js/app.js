ca9d4fcb398f7491a470e4a388e4ca181c52c95c
Sebastian <sebastian.tellez.o@gmail.com>
2015-07-15 13:34:18 -0500

    agregando nuestro propio ejemplo de tabs con ionic

---
+++ App9/www/js/app.js
@@ -0,0 +1,40 @@
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+    .state('tab',{
+      url: '/tab',
+      abstract: true,
+      templateUrl: 'templates/tabs.html'
+    })
+    .state('tab.home',{
+      url:'/home',
+      views:{
+        'tab-home':{
+          templateUrl:'templates/home.html',
+          controller:'homectrl'
+        }
+      }
+    }).state('tab.about',{
+      url:'/about',
+      views:{
+        'tab-about':{
+          templateUrl:'templates/about.html',
+          controller:'aboutctrl'
+        }
+      }
+    });
+
+  $urlRouterProvider.otherwise('/tab/home');
+
+});
---
+++ App9/www/js/controllers.js
@@ -0,0 +1,7 @@
+angular.module('starter.controllers',[])
+.controller('homectrl', function($scope) {
+  $scope.pagina = "Vista HOME"
+})
+.controller('aboutctrl', function($scope) {
+  $scope.pagina = "Vista ABOUT"
+})
