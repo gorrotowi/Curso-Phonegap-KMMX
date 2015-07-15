49f52b028b3c63a8f0deaee89910a16221d341bf
Sebastian <sebastian.tellez.o@gmail.com>
2015-07-15 14:14:45 -0500

    add vibration plugin

--- App1/platforms/firefoxos/www/cordova_plugins.js
+++ App1/platforms/firefoxos/www/cordova_plugins.js
@@ -11,52 +11,12 @@ module.exports = [
         "file": "plugins/cordova-plugin-battery-status/src/firefoxos/BatteryProxy.js",
         "id": "cordova-plugin-battery-status.Battery",
         "runs": true
-    },
-    {
-        "file": "plugins/cordova-plugin-device/www/device.js",
-        "id": "cordova-plugin-device.device",
-        "clobbers": [
-            "device"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-device/src/firefoxos/DeviceProxy.js",
-        "id": "cordova-plugin-device.DeviceProxy",
-        "runs": true
-    },
-    {
-        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
-        "id": "org.apache.cordova.dialogs.notification",
-        "merges": [
-            "navigator.notification"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.dialogs/src/firefoxos/notification.js",
-        "id": "org.apache.cordova.dialogs.dialogs-impl",
-        "runs": true
-    },
-    {
-        "file": "plugins/cordova-plugin-vibration/www/vibration.js",
-        "id": "cordova-plugin-vibration.notification",
-        "merges": [
-            "navigator.notification",
-            "navigator"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-vibration/src/firefoxos/VibrationProxy.js",
-        "id": "cordova-plugin-vibration.VibrationProxy",
-        "runs": true
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-battery-status": "1.0.0",
-    "cordova-plugin-device": "1.0.0",
-    "org.apache.cordova.dialogs": "0.3.0",
-    "cordova-plugin-vibration": "1.2.0"
+    "cordova-plugin-battery-status": "1.0.0"
 }
 // BOTTOM OF METADATA
 });
