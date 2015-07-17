4e0aba3672954a2c90d216c1bbabd4a1478e456d
Sebastian <sebastian.tellez.o@gmail.com>
2015-07-17 16:23:42 -0500

    agregando ejemplo con fb api

--- oauthapp/www/js/app.js
+++ oauthapp/www/js/app.js
@@ -17,6 +17,53 @@ angular.module('starter', ['ionic', 'ngCordova'])
     }
   });
 })
+.config(function($cordovaFacebookProvider) {
+  var appID = 816824881767080;
+  var version = "v2.4"; // or leave blank and default is v2.0
+  // $cordovaFacebookProvider.browserInit(appID, version);
+  if (window.cordova.platformId == "browser") {
+    facebookConnectPlugin.browserInit(appID, version);
+    // version is optional. It refers to the version of API you may want to use.
+}
+})
 .controller('mycontroller', function($scope) {
-  // body...
+
+  $scope.callfriends = function () {
+    alert('pull!')
+    setTimeout(function () {
+      $scope.$broadcast('scroll.refreshComplete');
+    }, 1500);
+    // $scope.$broadcast('scroll.refreshComplete');
+  }
+
+    document.addEventListener('deviceready', function() {
+
+      facebookConnectPlugin.login(
+        ["public_profile", "email", "user_friends","read_custom_friendlists"],
+        function(success) {
+          $scope.$apply(function() {
+            $scope.facebookdata = success
+          });
+          getFriends();
+      },
+      function(error) {
+        alert("error login "+error)
+      })
+
+    }, false);
+
+function getFriends() {
+  // facebookConnectPlugin.api('/'+$scope.facebookdata.authResponse.userID+"/friendlists?fields=name",["read_custom_friendlists"], function(successfriends) {
+  // facebookConnectPlugin.api('/me/friends',["read_custom_friendlists"], function(successfriends) {
+  facebookConnectPlugin.api('/me/taggable_friends?&limit=50',["read_custom_friendlists"], function(successfriends) {
+      $scope.$apply(function() {
+        $scope.friendlist = successfriends.data
+        $scope.friendlistpagin = successfriends
+        // $scope.userdatafb = successfriends
+      })
+  }, function (errorfriends) {
+    alert("error amigos "+errorfriends)
+  })
+}
+
 })
