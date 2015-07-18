c1948a21bb274ab69601cb01513075624bcf432f
Sebastian <sebastian.tellez.o@gmail.com>
2015-07-18 10:48:04 -0500

    Agregando ejemplo de facebook con pull e infinite scroll list

--- oauthapp/www/js/app.js
+++ oauthapp/www/js/app.js
@@ -1,14 +1,7 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
@@ -28,42 +21,78 @@ angular.module('starter', ['ionic', 'ngCordova'])
 })
 .controller('mycontroller', function($scope) {
 
+  $scope.friendlists = null
+  $scope.cancelmore = false
+
   $scope.callfriends = function () {
     alert('pull!')
     setTimeout(function () {
       $scope.$broadcast('scroll.refreshComplete');
     }, 1500);
-    // $scope.$broadcast('scroll.refreshComplete');
+    $scope.$broadcast('scroll.refreshComplete');
   }
 
     document.addEventListener('deviceready', function() {
-
       facebookConnectPlugin.login(
         ["public_profile", "email", "user_friends","read_custom_friendlists"],
         function(success) {
           $scope.$apply(function() {
             $scope.facebookdata = success
           });
-          getFriends();
+          $scope.$apply(function () {
+            $scope.cancelmore = true
+          })
+          $scope.loadMore();
       },
       function(error) {
         alert("error login "+error)
       })
-
     }, false);
 
-function getFriends() {
-  // facebookConnectPlugin.api('/'+$scope.facebookdata.authResponse.userID+"/friendlists?fields=name",["read_custom_friendlists"], function(successfriends) {
-  // facebookConnectPlugin.api('/me/friends',["read_custom_friendlists"], function(successfriends) {
-  facebookConnectPlugin.api('/me/taggable_friends?&limit=50',["read_custom_friendlists"], function(successfriends) {
-      $scope.$apply(function() {
-        $scope.friendlist = successfriends.data
-        $scope.friendlistpagin = successfriends
-        // $scope.userdatafb = successfriends
+    $scope.cancelmore = function () {
+      facebookConnectPlugin.getLoginStatus(function(succes) {
+        if (succes.status == 'connected'){
+          return true
+        }else {
+          return false
+        }
+      }, function (error) {
+        alert('fallo login status')
       })
-  }, function (errorfriends) {
-    alert("error amigos "+errorfriends)
-  })
-}
+    }
 
+    $scope.loadMore = function () {
+      if ($scope.friendlist == null) {
+        getFriends();
+      } else {
+        getMoreFriends($scope.friendlistpagin.paging.cursors.after);
+      }
+
+    }
+
+      function getFriends() {
+        // facebookConnectPlugin.api('/'+$scope.facebookdata.authResponse.userID+"/friendlists?fields=name",["read_custom_friendlists"], function(successfriends) {
+        // facebookConnectPlugin.api('/me/friends',["read_custom_friendlists"], function(successfriends) {
+        facebookConnectPlugin.api('/me/taggable_friends?&limit=50',["read_custom_friendlists"], function(successfriends) {
+            $scope.$apply(function() {
+              $scope.friendlist = successfriends.data
+              $scope.friendlistpagin = successfriends
+              $scope.$broadcast('scroll.infiniteScrollComplete');
+            })
+        }, function (errorfriends) {
+          alert("error amigos "+errorfriends)
+          $scope.$broadcast('scroll.infiniteScrollComplete');
+        })
+      }
+
+      function getMoreFriends(afterUrl) {
+        facebookConnectPlugin.api('/me/taggable_friends?&limit=50?after='+afterUrl,["read_custom_friendlists"], function(successfriends) {
+            $scope.$apply(function() {
+              $scope.friendlist = $scope.friendlist.concat(successfriends.data)
+              $scope.$broadcast('scroll.infiniteScrollComplete');
+            })
+        }, function (errorfriends) {
+          alert("error amigos "+errorfriends)
+        })
+      }
 })
