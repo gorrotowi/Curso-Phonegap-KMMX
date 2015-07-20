d41c03ac607e5308fedebd5715f290873dd80b58
Sebastian <sebastian.tellez.o@gmail.com>
2015-07-20 10:39:18 -0500

    add google sign project

---
+++ googlesign/www/js/app.js
@@ -0,0 +1,29 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic', 'ngCordova'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+.controller('googlectrl', function($scope, $cordovaOauth) {
+  $scope.googleLogin = function() {
+    $cordovaOauth.google("59103173414-ffj1jlms0qoev93lqceb41jj4rogn9ek.apps.googleusercontent.com", ["https://www.googleapis.com/auth/urlshortener", "https://www.googleapis.com/auth/userinfo.email"])
+      .then(function(success) {
+        alert(JSON.stringify(success))
+      }, function(error) {
+        alert("error"+error)
+      });
+  }
+})
